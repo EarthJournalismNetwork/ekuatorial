3f36490429d1ce8d8920593b804c281677431e9b
Miguel Peixe <miguel@cardume.art.br>
2013-11-20 20:01:12 -0200

    fix single map

--- single-map.php
+++ single-map.php
@@ -30,18 +30,31 @@
 	query_posts($query);
 	if(have_posts()) : ?>
 
-
-		<div class="container">
-			<div class="twelve columns">
-				<?php get_search_form(); ?>
-			</div>
-			<section id="last-stories" class="loop-section">
-				<div class="twelve columns">
-					<h3><?php _e('Stories on', 'infoamazonia'); ?> &ldquo;<?php the_title(); ?>&ldquo;</h3>
-				</div>
-				<?php get_template_part('loop'); ?>
-			</section>
-		</div>
+        <section id="last-stories" class="loop-section">
+            <div class="section-title">
+                <div class="container">
+                    <div class="twelve columns">
+                        <h3><?php _e('Stories on', 'infoamazonia'); ?> &ldquo;<?php the_title(); ?>&ldquo;</h3>
+                        <div class="query-actions">
+                            <?php
+                            global $wp_query;
+                            $args = $wp_query->query;
+                            $args = array_merge($args, $_GET);
+                            $geojson = jeo_get_api_url($args);
+                            $download = jeo_get_api_download_url($args);
+                            $rss = add_query_arg(array('feed' => 'rss'));
+                            ?>
+                            <a class="rss" href="<?php echo $rss; ?>"><?php _e('RSS Feed', 'ekuatorial'); ?></a>
+                            <a class="geojson" href="<?php echo $geojson; ?>"><?php _e('Get GeoJSON', 'ekuatorial'); ?></a>
+                            <a class="download" href="<?php echo $download; ?>"><?php _e('Download', 'ekuatorial'); ?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <?php get_template_part('loop'); ?>
+            </div>
+        </section>
 
 	<?php
 	endif;
