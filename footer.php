aebf236b5b981b799380eed049169e9662a50280
Tob <surachartsomma@gmail.com>
2018-06-22 12:36:09 +0700

    Update footer logo

--- footer.php
+++ footer.php
@@ -3,12 +3,11 @@
 	<ul class="nav clearfix">
 		<!-- <li class="label by">Project by</li> -->
 		<!--<li class="label supported">Supported by</li> -->
-		<li><a class="icon siej" href="http://www.siej.or.id/" target="_blank">Society of Indonesian Environmental Journalists</a></li>
+		<li><a class="icon siej" href="http://www.siej.or.id" target="_blank">Society of Indonesian Environmental Journalists</a></li>
 		<li><a class="icon internews" href="http://www.internews.org" target="_blank">Internews</a></li>
-		<li><a class="icon packard" href="http://www.packard.org/" target="_blank">Packard Foundation</a></li>
-		<li><a class="icon ecolab" href="http://ecolab.oeco.org.br/" target="_blank">EcoLab</a></li>
+		<li><a class="icon ejn" href="https://earthjournalism.net" target="_blank">Earth Journalism Network</a></li>
 	</ul>
 </footer>
 <?php wp_footer(); ?>
 </body>
-</html>
+</html
