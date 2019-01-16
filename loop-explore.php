07219f1a84fbea31c2506dc5a3badd428abf7327
Tob <surachartsomma@gmail.com>
2019-01-16 16:36:45 +0700

    Fix wrong syntax

--- loop-explore.php
+++ loop-explore.php
@@ -24,9 +24,9 @@ wp_enqueue_script('lockfixed', get_stylesheet_directory_uri() . '/js/jquery.lock
 							</a>
 							<h2>
 								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-								<?php if (get_post_meta($post->ID, 'is_label', true) == "1") ?>
+								<?php if (get_post_meta($post->ID, 'is_label', true) == "1"): ?>
 								<a href="https://earthjournalism.net/"><span class="label">Belt, Road & the Environment</span></a>
-								<?php endif ?>
+								<?php endif; ?>
 							</h2>
 							<p class="meta clearfix">
 								<span class="date">
