40ed77d9175cf7c288a08d225918e4075101d265
bwstefano <stefanobw123@gmail.com>
2019-08-07 22:34:39 -0300

    Deleted everything to start fresh

--- README.md
+++
@@ -1,7 +0,0 @@
-Ekuatorial
-==========
-
-WordPress JEO child theme
--------------------------
-
-Install this theme using [JEO parent theme](http://github.com/cardume/jeo)
--- section-main-widget.php
+++
@@ -1,9 +0,0 @@
-<?php if(is_active_sidebar('main-sidebar')) : ?>
-	<aside id="main-widgets">
-		<div class="container">
-			<div class="widgets">
-				<?php dynamic_sidebar('main-sidebar'); ?>
-			</div>
-		</div>
-	</aside>
-<?php endif; ?>
